9f109c773b0079966c0a02f7b9c1329e37699f31
Kone madou <madoumadeltitokone77@gmail.com>
2025-07-23 15:39:24 +0000

    correction

--- src/main/java/com/odk/securityConfig/Security.java
+++ src/main/java/com/odk/securityConfig/Security.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,61 +25,13 @@ import static org.springframework.http.HttpMethod.GET;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @AllArgsConstructor
 public class Security {
 
     private JwtFilter jwtFilter;
     private UserDetailsService userDetailsService;
     private PasswordEncoder passwordEncoder;
-
-   /* @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Ajouter un filtre JWT avant le filtre d'authentification par formulaire
-        // http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
-        // Configurer les autorisations
-        http
-                .cors().and().csrf().disable()  // Désactiver CSRF pour les APIs REST
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // On utilise des tokens, pas des sessions
-                .and()
-                .authorizeHttpRequests( requests -> requests
-                        // Autoriser l'accès à la création d'organisation et d'utilisateur sans authentification
-                        .requestMatchers("/auth/**").permitAll()  // Autoriser les routes d'authentification
-                        .requestMatchers("/participant/**").permitAll()  // Autoriser les routes d'authentification
-                        .requestMatchers("/personnel/**").permitAll()  // Autoriser les routes d'authentification
-                        .requestMatchers("/api/import/**").permitAll()  // Autoriser les routes d'authentification
-                        .requestMatchers("/etape/{id}/participants/upload").permitAll()
-                        .requestMatchers("/etape/**").permitAll()
-                        .requestMatchers("/activite/**").permitAll()
-                        .requestMatchers("/activite/enCours").permitAll()
-                        .requestMatchers("/vigile/**").permitAll()
-                        .requestMatchers("/critere/**").permitAll()
-                        .requestMatchers("/entite/**").permitAll()
-                        .requestMatchers("/images/**").permitAll()
-                        .requestMatchers("/typeActivite/**").permitAll()
-                        .requestMatchers("/utilisateur/**").permitAll()
-                        .requestMatchers("/utilisateur/change-password").authenticated()
-                        .requestMatchers("/reporting/**").permitAll()
-                        .requestMatchers("/role/**").permitAll()
-                        // Autoriser les routes d'authentification
-                      // Autoriser les routes d'authentification
-                        .anyRequest().authenticated() // Toutes les autres requêtes nécessitent une authentification
-                );
-//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
-
-        return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
